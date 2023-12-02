ce4af8fe889b25cb2f28b14b96d93cdc9147aa84
Sahar Refaei <saharrefaei1999@gmail.com>
2023-12-02 21:19:02 +0100

    delete user done

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -10,15 +10,35 @@ import Paper from "@mui/material/Paper";
 import { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
 import Button from "@mui/material/Button";
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
 function GetUsers() {
   const [users, setusers] = useState([]);
-  console.log(users, "users");
-  useEffect(async() => {
-   await fetch("https://fir-75f9a-default-rtdb.firebaseio.com/Users.json")
-      .then((response) => response.json())
-      .then((data) => setusers(Object.entries(data)));
-  }, []);
+  const [refresh,setrefresh]=useState(false)
+
+
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://fir-75f9a-default-rtdb.firebaseio.com/Users.json");
+      const data = await response.json();
+      setusers(Object.entries(data));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  fetchData();
+}, [refresh]);
+
+const RemoveHandler =async(userId)=>{
+ await fetch(`https://fir-75f9a-default-rtdb.firebaseio.com/Users/${userId}.json`, {
+method:'DELETE'
+
+}).then(response=>console.log(response))
+setrefresh(prev => !prev)
+
+}
 
   return (
     <GetUsersContainer>
@@ -29,6 +49,7 @@ function GetUsers() {
               <TableCell>First Name</TableCell>
               <TableCell align="center">Last Name</TableCell>
               <TableCell align="center">Email</TableCell>
+              <TableCell > Options </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -37,6 +58,12 @@ function GetUsers() {
                 <TableCell component="th" scope="row">{user[1].FristName}</TableCell>
                 <TableCell align="center">{user[1].lastName}</TableCell>
                 <TableCell align="center">{user[1].email}</TableCell>
+                <TableCell><DeleteIcon onClick={()=>{
+                        RemoveHandler(user[0])
+                }
+           
+            }/>
+                 <BorderColorIcon className='BorderColorIcon'/></TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -55,8 +82,10 @@ export default GetUsers;
 
 const GetUsersContainer = styled.div`
 .BackMainPage{
-
     margin-top: 5%;
+}
+.BorderColorIcon{
+    margin-left: 15%;
 }
 
 `
